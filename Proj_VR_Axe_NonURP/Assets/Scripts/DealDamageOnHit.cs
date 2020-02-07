60a28ecd0d0acac3922a902ff190b2bce087f8a7
FTWRahul <r.poundarik@gmail.com>
2020-02-06 20:03:58 -0500

    Damage deal script

---
+++ Proj_VR_Axe_NonURP/Assets/Scripts/DealDamageOnHit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealDamageOnHit : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.GetComponent<IReceiveDamage>() != null)
+        {
+            other.collider.GetComponent<IReceiveDamage>().GetHit();
+        }
+    }
+}
