912982de53061ef8c8a88c2f0fec613abc360ce5
Jane <glazychevaeo@gmail.com>
2020-02-06 19:35:36 -0500

    Enemy prototype

---
+++ Proj_VR_Axe/Assets/Enemies/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour, IReceiveDamage
+{
+    public Transform spot;
+    public Events.EventEnemyDeath onEnemyDeath;
+    
+    public void GetHit()
+    {
+        Destroy(gameObject);
+        onEnemyDeath?.Invoke(spot);
+    }
+}
+
+public interface IReceiveDamage
+{
+    void GetHit();
+}
