bc4877d5fb1448bf01da249b63852084be9e0746
FTWRahul <r.poundarik@gmail.com>
2020-02-13 19:49:33 -0500

    Samll changes

---
+++ Proj_VR_Axe_NonURP/Assets/Scripts/SpawnWeapon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class SpawnWeapon : MonoBehaviour
+{
+    public GameObject prefab;
+    public Transform target;
+
+    [ContextMenu("Do it")]
+    public void SpawnWeaponNow()
+    {
+        Instantiate(prefab, target.position, Quaternion.identity);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SpawnWeaponNow();
+        }
+    }
+}
