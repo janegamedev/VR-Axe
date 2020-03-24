b07bc2a3269508cf313bfc6980ca12a3b599d33e
FTWRahul <r.poundarik@gmail.com>
2020-03-24 16:14:50 -0400

    Revert "Kas_Reopening on new computer"
    This reverts commit 18a63680acda328066705ad8cea14dea0aee755c.

---
+++ Proj_VR_Axe/Assets/Enemies/Scripts/EnemyManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform[] spawnPositions;
+    public float timeForRespawn;
+
+    private void Start()
+    {
+        foreach (var spawn in spawnPositions)
+        {
+            SpawnEnemy(spawn);
+        }
+    }
+
+    private void SpawnEnemy(Transform spot)
+    {
+        EnemyBehaviour enemy = Instantiate(enemyPrefab, spot.position, Quaternion.identity).GetComponent<EnemyBehaviour>();
+        enemy.spot = spot;
+        enemy.onEnemyDeath.AddListener(EnemyDeath);
+    }
+
+    private void EnemyDeath(Transform spot)
+    {
+        StartCoroutine(SpawnEnemyWithDelay(spot));
+    }
+
+    private IEnumerator SpawnEnemyWithDelay(Transform spot)
+    {
+        yield return new WaitForSeconds(timeForRespawn);
+        
+        SpawnEnemy(spot);
+    }
+}
---
+++ Proj_VR_Axe_NonURP/Assets/Scripts/ObjcetReturnToHand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class ObjcetReturnToHand : MonoBehaviour
+{
+    public GameObject target;
+    public SteamVR_Action_Boolean retractAction;
+    private Rigidbody rb;
+
+    public float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        target = FindObjectOfType<Hand>().gameObject;
+
+    }
+
+    private void Update()
+    {
+        ReturnToHand();
+    }
+
+    public void ReturnToHand()
+    {
+        if (retractAction[SteamVR_Input_Sources.RightHand].state)
+        {
+            Debug.Log("BOOP");
+
+            var dir = target.transform.position - transform.position;
+            //rb.velocity = Vector3.zero;
+            rb.AddForce(dir.normalized * speed * Time.deltaTime);
+            
+            
+        }
+    }
+}
