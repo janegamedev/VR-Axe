b43589c419e9aca37f8a9af5ffb0996fb3c5b2b2
EyepatchDoge <kaspo.eyepatch@gmail.com>
2020-03-26 17:09:12 -0400

    kas_updatetomainmenu
    - Started to implement settings pannel
    - added a credits pannel
    -updated script for leaving game and changing scenes

---
+++ Proj_VR_Axe_NonURP/Assets/Scripts/SceneManaging.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Kas made this script
+public class SceneManaging : MonoBehaviour
+{
+    public string sceneName;
+
+    public void ChangeTheScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+    //quit game
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        Debug.Log("Game has quitted");
+    }
+}
