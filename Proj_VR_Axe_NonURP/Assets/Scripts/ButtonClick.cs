9e2611c07a1371fd1a4a3ac0f262bef62fd40ab5
EyepatchDoge <kaspo.eyepatch@gmail.com>
2020-03-29 18:23:19 -0400

    Kas_UpdaingMainMenu
    - added audio to button press for better player input
    - made more changes to the main menu scene

---
+++ Proj_VR_Axe_NonURP/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    public AudioSource buttonPress;
+
+    public void PlaySound()
+    {
+        buttonPress.Play();
+    }
+}
