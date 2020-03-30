c86b3b3d4c2bac3b62f3e06d7455984e110470bc
FTWRahul <r.poundarik@gmail.com>
2020-03-29 22:21:54 -0400

    Nav mesh agents basics
    Agents move on dedicated positions.

--- Proj_VR_Axe_NonURP/Assets/Scripts/EnemyBehaviour.cs
+++ Proj_VR_Axe_NonURP/Assets/Scripts/EnemyBehaviour.cs
@@ -1,12 +1,66 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour, IReceiveDamage
 {
     public Transform spot;
     public Events.EventEnemyDeath onEnemyDeath;
-    
+    private NavMeshAgent agent;
+
+    public Transform tempEndLocation;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+//        NavMeshHit closestHit;
+//        if( NavMesh.SamplePosition(  transform.position, out closestHit, 500, 1 ) ){
+//            transform.position = closestHit.position;
+//            //go.AddComponent<NavMeshAgent>();
+//            //TODO
+//        }
+//        else{
+//            Debug.Log("...");
+//        }
+        Init(tempEndLocation.position);
+    }
+
+    public void Init(Vector3 position)
+    {
+        if (agent.enabled)
+        {
+            if (agent.isOnNavMesh)
+            {                
+                Debug.Log("Setting destination");
+                agent.SetDestination(position);
+            }
+            else
+            {
+                Debug.Log("Boop");
+                //agent.SetDestination(position);
+            }
+        }
+    }
+
+    private void Update()
+    {
+//        if (!agent.isOnNavMesh)
+//        {
+//            agent.enabled = false;
+//            agent.enabled = true;
+//            Init(tempEndLocation.position);
+//        }
+
+
+        
+    }
+
     public void GetHit()
     {
         Destroy(gameObject);
