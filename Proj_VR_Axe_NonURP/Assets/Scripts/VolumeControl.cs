359467b313c8aaba060c5eb201666a354a63fb06
EyepatchDoge <kaspo.eyepatch@gmail.com>
2020-04-01 17:27:39 -0400

    Kas_updatingsettings menu
    I did a thing!!!!!!!

--- Proj_VR_Axe_NonURP/Assets/Scripts/VolumeControl.cs
+++ Proj_VR_Axe_NonURP/Assets/Scripts/VolumeControl.cs
@@ -9,22 +9,25 @@ public class VolumeControl: SingletonManager<VolumeControl>
     
     public void SetLevel (float sliderValue)
     {
-        //mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
-        mixer.SetFloat("MusicVol", sliderValue);
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+        //mixer.SetFloat("MusicVol", sliderValue);
     }
 
     public float GetVolume()
     {
         float value;
-        bool result = mixer.GetFloat("masterVol", out value);
+        bool result = mixer.GetFloat("MusicVol", out value);
 
         if (result)
         {
-            return value;
+            Debug.Log(Mathf.Pow(10, value) * 20);
+            return Mathf.Pow(10, value) * 20;
+            
         }
 
         else
         {
+            Debug.Log("Set to zero");
             return 0f;
         }
     }
